494ad27abda189cf1bb257ee2fe90e112e53eaa1
Yuichiro Tsuchiya <t.yic.yt@gmail.com>
2021-04-25 21:11:02 +0900

    Remove unused deps and their imports

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -1,6 +1,4 @@
-import argparse
 import imutils
-import time
 import cv2
 import numpy as np
 
@@ -15,13 +13,13 @@ def style_transfer(image, model):
     blob = cv2.dnn.blobFromImage(image, 1.0, (w, h), (103.939, 116.779, 123.680), swapRB=False, crop=False)
     model.setInput(blob)
     output = model.forward()
-        
+
     output = output.reshape((3, output.shape[2], output.shape[3]))
     output[0] += 103.939
     output[1] += 116.779
     output[2] += 123.680
     output /= 255.0
     output = output.transpose(1, 2, 0)
-    output = np.clip(output, 0.0, 1.0)    
+    output = np.clip(output, 0.0, 1.0)
     output = imutils.resize(output, width=500)
     return output
