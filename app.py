5b9a49e93d93857b419a7179c8fcd907534931b9
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2021-05-11 22:58:47 +0900

    Fix menu order

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from input import image_input, webcam_input
 
 st.title("Neural Style Transfer")
 st.sidebar.title('Navigation')
-method = st.sidebar.radio('Go To ->', options=['Image', 'Webcam'])
+method = st.sidebar.radio('Go To ->', options=['Webcam', 'Image'])
 st.sidebar.header('Options')
 
 style_model_name = st.sidebar.selectbox("Choose the style model: ", style_models_name)
